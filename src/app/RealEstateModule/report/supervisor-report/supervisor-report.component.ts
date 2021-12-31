e0c0b1e85ad8ca049e5b4a29ebe6b202403f5203
MahmoudAbdElrdy <47526228+MahmoudAbdElrdy@users.noreply.github.com>
2021-12-31 16:41:16 +0200

    report

---
+++ src/app/RealEstateModule/report/supervisor-report/supervisor-report.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { PublicService } from 'src/app/shared/services/public.service';
+import { ResponseData } from 'src/app/shared/models/ResponseData';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReportService } from 'src/app/shared/services/report.service';
+@Component({
+  selector: 'app-supervisor-report',
+  templateUrl: './supervisor-report.component.html',
+  styleUrls: ['./supervisor-report.component.css']
+})
+export class SupervisorReportComponent implements OnInit {
+  dataDropDown: any;
+  FieldsProject: Object = { text: 'text', value: 'value' };
+  constructor(  private _service: ReportService,private _publicService: PublicService,
+    private formBuilder: FormBuilder,) { }
+
+  ngOnInit(): void {
+    this.getDropDownList();
+    this.form = this.formBuilder.group({
+      supervisorId:  [null, [Validators.required]],
+      fromDate: [null],
+      toDate: [null],
+    });
+  }
+  getDropDownList() {
+
+    this._publicService.get("Supervisor/GetDropDownList")
+      .subscribe((res: ResponseData) => {
+        if (res.isSuccess == true) {
+
+          this.dataDropDown = res.data;
+
+        }
+      });
+  }
+  form: FormGroup;
+  getFile() {
+    
+    this.form.markAllAsTouched();
+    if(this.form.valid){
+     
+       
+        this._service.reportSupervisor(this.form.value).subscribe((data: Blob) => {
+              var fileType: any;
+            fileType = "application/pdf";
+            var blob = new Blob([data], { type: fileType });
+            const objectUrl: string = URL.createObjectURL(blob);
+       
+            window.open(objectUrl, '_blank');
+         
+        })
+        }}
+ 
+}
